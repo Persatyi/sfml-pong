46c1a5c5a3604642e8a4b71b8a1e1ab1fc0bd7e3
Володимир Іванченко <persatyi@ukr.net>
2026-07-23 23:10:25 +0300

    little update

--- include/GameEnums.h
+++ include/GameEnums.h
@@ -2,7 +2,7 @@
 #define GAME_ENUMS_H_
 
 // стан для визначення чи відкривати меню чи гру
-enum class GameState { Menu, Playing, WaitingToStart, Quit };
+enum class GameState { Menu, Playing, WaitingToStart, Pause, Quit };
 // стан для визначення який пунк меню було вибрано
 enum MenuItem { Start, Quit };
 
